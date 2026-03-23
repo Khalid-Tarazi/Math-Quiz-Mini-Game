b90345b9df6e49b1fb2892d8f744369119234f62
UNDAMAG3D <k.tarazi@yahoo.com>
2026-03-23 11:29:36 +0300

    Refactor: modularize math quiz logic and add input helpers
    Added structs for questions and quizzes, input validation functions, random number and operation generators, and a simple calculator. Enhanced question generation to support mixed levels and operations. Updated playMathGame to use new helpers, improving code organization and flexibility. Adopted 'using namespace std' for brevity.

--- Math-Quiz-Mini-Game.cpp
+++ Math-Quiz-Mini-Game.cpp
@@ -8,13 +8,23 @@ enum enQuestionsLevel {Easy = 1, Medium = 2, Hard = 3, Mix = 4};
 enum enOperationType {Add = 1, Sub = 2, Mul = 3, Div = 4, MixOp = 5};
 
 struct stQuestion {
-
-
+    int number1 = 0;
+    int number2 = 0;
+    enOperationType opeartionType;
+    enQuestionsLevel questionLevel;
+    int correctAnswer = 0;
+    int playerAnswer = 0;
+    bool answerResult = false;
 };
 
 struct stQuizz {
-
-
+    stQuestion questionList[100];
+    short numberOfQuestions;
+    enQuestionsLevel questionLevel;
+    enOperationType opType;
+    short NumberOfWrongAnswers = 0;
+    short NumberOfCorrectAnswers = 0;
+    bool isPass = false;
 };
 
 string getOpTypeSymbol(enOperationType opType) {
@@ -27,7 +37,7 @@ string getQuestionLevelText(enQuestionsLevel questionLevel) {
 }
 
 int randomNumb(int from, int to) {
-
+    return rand() % (To - From + 1) + From;
 }
 
 void setScreenColor(bool right) {
@@ -35,31 +45,102 @@ void setScreenColor(bool right) {
 }
 
 short readHowManyQuestions() {
+    short numberOfQuestions;
 
+    do {
+        cout << "How many questions do you want to answer? ";
+        cin >> numberOfQuestions;
+    } while (numberOfQuestions < 1 || numberOfQuestions > 10);
+    return numberOfQuestions;
 }
 
 enQuestionsLevel readQuestionLevelType() {
+    short questionLevel = 0;
 
+    do {
+        cout << "Enter question level: [1]Easy, [2] Medium, [3]Hard, [4]Mix ? ";
+        cin >> questionLevel;
+    } while (questionLevel < 1 || questionLevel > 4);
+    
+    return (enQuestionsLevel)questionLevel;
 }
 
 enOperationType readOpType() {
+    short opType;
 
+    do {
+        cout << "Enter Opearation type: [1]add, [2]sub, [3]mul, [4]div, [5]mixOp ? ";
+        cin >> opType;
+    } while (opType < 1 || opType > 5);
+
+    return (enOperationType)opType;
 }
 
 int simpleCalculator(int number1, int number2, enOperationType opType) {
-
+    switch (opType) {
+    case enOperationType::Add:
+        return number1 + number2;
+    case enOperationType::Sub:
+        return number1 - number2;
+    case enOperationType::Mul:
+        return number1 * number2;
+    case enOperationType::Div:
+        return number1 / number2;
+    default:
+        return number1 + number2;
+    }
 }
 
 enOperationType getRandomOperationType() {
-
+    int op = randomNumb(1, 4);
+    return (enOperationType)op;
 }
 
 stQuestion generateQuestion(enQuestionsLevel questionLevel, enOperationType opType) {
+    stQuestion question;
 
+    if (questionLevel == enQuestionsLevel::Mix) {
+        questionLevel = (enQuestionsLevel)randomNumb(1, 3);
+    }
+
+    if (opType == enOperationType::MixOp) {
+        opType = getRandomOperationType();
+    }
+
+    question.opeartionType = opType;
+
+    switch (questionLevel) {
+    
+    case enQuestionsLevel::Easy:
+        question.number1 = randomNumb(1, 10);
+        question.number2 = randomNumb(1, 10);
+
+        question.correctAnswer = simpleCalculator(question.number1, question.number2, question.opeartionType);
+        question.questionLevel = questionLevel;
+        return question;
+
+    case enQuestionsLevel::Medium:
+        question.number1 = randomNumb(10, 50);
+        question.number2 = randomNumb(10, 50);
+
+        question.correctAnswer = simpleCalculator(question.number1, question.number2, question.opeartionType);
+        question.questionLevel = questionLevel;
+        return question;
+
+    case enQuestionsLevel::Hard:
+        question.number1 = randomNumb(50, 100);
+        question.number2 = randomNumb(50, 100);
+
+        question.correctAnswer = simpleCalculator(question.number1, question.number2, question.opeartionType);
+        question.questionLevel = questionLevel;
+        return question;
+    }
 }
 
 void generateQuizzQuestions(stQuizz& quizz) {
-
+    for (short question = 0; question < quizz.numberOfQuestions; question++) {
+        quizz.questionList[question] = generateQuestion(quizz.questionLevel, quizz.opType);
+    }
 }
 
 int readQuestionAnswer() {
@@ -87,7 +168,12 @@ void printQuizzResult(stQuizz& quizz) {
 }
 
 void playMathGame() {
+    stQuizz quizz;
+    quizz.numberOfQuestions = readHowManyQuestions();
+    quizz.questionLevel = readQuestionLevelType();
+    quizz.opType = readOpType();
     
+    generateQuizzQuestions(quizz);
 
 
 }
